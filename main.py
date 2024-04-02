1064808804ae803b77bb4ae55730d260accd8821
Naga Venkata Gopi Prudhvi Maddina <prudhvi@Nagas-MacBook-Air.local>
2024-04-01 20:56:25 -0500

    Added the functionality to draw CFG's using graphviz library and sending Dot string of the code snippet as input

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 from llama_index.readers.web import SimpleWebPageReader
 from llama_index.core.indices.vector_store import VectorStoreIndex
 
+from CFGImageGenerator import CFGImageGenerator
+
 code_string = '''
 def fun1():
     a = 1
@@ -29,8 +31,16 @@ def main(url:str) -> None:
     documents = SimpleWebPageReader(html_to_text=True).load_data(urls=[url])
     index = VectorStoreIndex.from_documents(documents=documents)
     query_engine = index.as_query_engine()
-    response = query_engine.query("Give the control flow diagram for the code snippet: " + code_string)
+    response = query_engine.query("Give me the dot string response of the code snippet. "
+                                  "I want to draw the control flow diagram for it. "
+                                  "I will directly feed it to the graphviz to generate png of control flow diagram"
+                                  "Make sure you don't add any thing other than the connection strings" + code_string)
+
+
     print(response)
+
+    cfg = CFGImageGenerator()
+    cfg.generate_image(response.__str__())
 
 
 if __name__ == '__main__':
